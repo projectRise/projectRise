66f5ce00b35157daae93c36f7f4f7a69f6092a09
albrdev <albrdev@gmail.com>
2020-02-28 15:04:31 +0100

    test

--- projectRise.cpp
+++ projectRise.cpp
@@ -44,7 +44,7 @@ Weather humiditySensor;     //Create an instance of the humidity sensor
 
 RTC_DS3231 rtc;
 
-LightTracker lt(10, 9, A2, A3, A0, A1);
+LightTracker lightTracker(44, 45, A8, A9, A10, A11);
 
 //Hardware pin definitions
 //-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
@@ -125,6 +125,9 @@ void setup(void)
         }
     }
 
+    DebugPrintLine("Light tracker...");
+    lightTracker.Begin();
+
     DebugPrintLine("Weather shield...");
     pinMode(PIN_STAT_BLUE, OUTPUT); //Status LED Blue
     pinMode(PIN_STAT_GREEN, OUTPUT); //Status LED Green
@@ -187,28 +190,6 @@ void setup(void)
 
 void loop(void)
 {
-    // Enter idle state for 8 s with the rest of peripherals turned off
-    // Each microcontroller comes with different number of peripherals
-    // Comment off line of code where necessary
-
-    // ATmega328P, ATmega168
-    //LowPower.idle(SLEEP_8S, ADC_OFF, TIMER2_OFF, TIMER1_OFF, TIMER0_OFF, 
-    //              SPI_OFF, USART0_OFF, TWI_OFF);
-
-    // ATmega32U4
-    //LowPower.idle(SLEEP_8S, ADC_OFF, TIMER4_OFF, TIMER3_OFF, TIMER1_OFF, 
-    //		  TIMER0_OFF, SPI_OFF, USART1_OFF, TWI_OFF, USB_OFF);
-
-    // ATmega2560
-    //LowPower.idle(SLEEP_2S, ADC_OFF, TIMER5_OFF, TIMER4_OFF, TIMER3_OFF, 
-    //        TIMER2_OFF, TIMER1_OFF, TIMER0_OFF, SPI_OFF, USART3_OFF, 
-    //        USART2_OFF, USART1_OFF, USART0_OFF, TWI_OFF);
-
-    // ATmega256RFR2
-    //LowPower.idle(SLEEP_8S, ADC_OFF, TIMER5_OFF, TIMER4_OFF, TIMER3_OFF, 
-    //      TIMER2_OFF, TIMER1_OFF, TIMER0_OFF, SPI_OFF,
-    //      USART1_OFF, USART0_OFF, TWI_OFF);
-
     unsigned long now = millis();
     if((long)(now - nextUpdate) >= 0)
     {
@@ -216,9 +197,10 @@ void loop(void)
         nextUpdate += UPDATE_INTERVAL;
     }
 
-    lt.Poll();
-    sleep.pwrDownMode(); //set sleep mode
-    sleep.sleepDelay(sleepTime); //sleep for: sleepTime
+    lightTracker.Poll();
+    delay(35);
+    //sleep.pwrDownMode(); //set sleep mode
+    //sleep.sleepDelay(sleepTime); //sleep for: sleepTime
 }
 
 #define TEST_READOFFSET     0U                                          // Position in the file to start reading (should be even divisible by size of 'collection_t').
